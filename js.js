95fa58c1dfa90348f8718780196154541d95cbce
Ahmad Alhashimi <ahmad.alhashimi83@gmail.com>
2020-08-20 13:27:43 +0200

    JavaScript

---
+++ js.js
@@ -0,0 +1,27 @@
+// // // Integer Numbers
+// // // 2 , 5 , 7
+// // console.log(14)
+// // // String
+// // // '', ""
+// // console.log('Sam')
+// // // Array 
+// // // [10, 5, 3, "HTML", "CSS"]
+// // console.log([10, 5, 3, "HTML", "CSS"])
+// // // object
+// // //  {}
+// // console.log({
+// //     'name': 'sam',
+// //     'city' : 'Rotterdam',
+// //     'age': 27,
+// //     'hobbies': ['football', 'pizza'],
+// //     'isAdmin': false
+// // })
+// // //  Boolean
+// // // True, False
+// console.log(true)
+// name = 'css'
+var first_Name = [10, 5, 3, "HTML", "CSS", ['football', 'pizza'], false]
+console.log(first_Name)
+
+console.log(typeof(first_Name[0]), typeof(first_Name[4]), typeof(first_Name[5]), typeof(first_Name[6]))
+console.log(first_Name.length)
